5433461b6dd595a042de84886fa081546843951d
CookDas2 <parksw125368@naver.com>
2021-09-04 02:02:47 +0900

    210904

--- app/src/main/java/com/kotlin/a05_pictureframe/MainActivity.kt
+++ app/src/main/java/com/kotlin/a05_pictureframe/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -14,6 +15,17 @@ class MainActivity : AppCompatActivity() {
     }
     private val startPhotoFrameModeButton : Button by lazy {
         findViewById<Button>(R.id.startPhotoFrameModeButton)
+    }
+    private val listImageView : List<ImageView> by lazy{
+        mutableListOf<ImageView>().apply{
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +44,7 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED ->{ //권한이 잘 부여되어 있음.
                     //기능
+
                 }
                 // 권한이 거절되었을 때 교육용 팝업을 띄울 필요가 있는지 체크
                 // boolean
@@ -46,14 +59,37 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1000 ->{
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //true : 권한이 부여됨
+                    //기능 구현
+                    navigatePhotos()
+                }else{
+                    Toast.makeText(this,"권한을 거부하셨습니다. ",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else ->{}
+
+        }
+    }
+    private fun navigatePhotos(){
+
+    }
+
+
     private fun showPermissionContextPopup(){
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
             .setMessage("전자액자에 앱에서 사지을 불러오기 위해 권한이 필요합니다. ")
-            .setPositiveButton("동의하기", {dialog, which ->
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
-            })
-            .setNegativeButton("취소하기") { _, _ -> }
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+                .setNegativeButton("취소하기") { _, _ -> }
+                .create()
+                .show()
     }
     private fun initStartPhotoFrameModeButton(){
 
