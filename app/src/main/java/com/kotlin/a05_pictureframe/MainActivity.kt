7bc8fd802a691198a7297b27878eb12eaaf48f5d
CookDas2 <parksw125368@naver.com>
2021-09-05 13:27:14 +0900

    210905

--- app/src/main/java/com/kotlin/a05_pictureframe/MainActivity.kt
+++ app/src/main/java/com/kotlin/a05_pictureframe/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.kotlin.a05_pictureframe
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,6 +11,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val addPhotoButon : Button by lazy {
@@ -27,16 +31,23 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    val imageUriList : MutableList<Uri> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initAddPhotoButton()//버튼을 초기화 해준다. 한눈에 보기좋게 fun 으로 뺀다.
         initStartPhotoFrameModeButton()
+
     }
 
     private fun initAddPhotoButton(){
         addPhotoButon.setOnClickListener {
+            if (imageUriList.size >= 6){
+                Toast.makeText(this,"이미 사진이 꽉 찼습니다.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             when{
                 //권한이 등록되어 있는가?
                 ContextCompat.checkSelfPermission(
@@ -44,7 +55,7 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED ->{ //권한이 잘 부여되어 있음.
                     //기능
-
+                    navigatePhotos()
                 }
                 // 권한이 거절되었을 때 교육용 팝업을 띄울 필요가 있는지 체크
                 // boolean
@@ -58,6 +69,17 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+    private fun initStartPhotoFrameModeButton(){
+        startPhotoFrameModeButton.setOnClickListener {
+            val intent = Intent(this,PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed { index, uri ->
+                intent.putExtra("photo${index}",uri.toString())
+            }
+            intent.putExtra("photoListSize",imageUriList.size)
+            startActivity(intent)
+        }
+
     }
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -76,7 +98,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun navigatePhotos(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent,2000)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK){ //취소한경우
+            return
+        }
+        when (requestCode){
+            2000 -> {
+                val selectedImageUri = data?.data
+                if(selectedImageUri == null){
+                    Toast.makeText(this,"사진을 가져오지 못했습니다",Toast.LENGTH_SHORT).show()
+                }else{
+                    imageUriList.add(selectedImageUri)
+                    listImageView[imageUriList.size-1].setImageURI(selectedImageUri) // ImageView에 집어넣기
+                    Toast.makeText(this,"${imageUriList.size}",Toast.LENGTH_SHORT).show()
+                }
+
+            }else -> {
+                Toast.makeText(this,"사진을 가져오지 못했습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
@@ -90,8 +137,5 @@ class MainActivity : AppCompatActivity() {
                 .setNegativeButton("취소하기") { _, _ -> }
                 .create()
                 .show()
-    }
-    private fun initStartPhotoFrameModeButton(){
-
     }
 }
